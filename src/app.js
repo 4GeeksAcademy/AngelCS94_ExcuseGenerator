bc41d0fc8e7ab6422886b7b7e8fe4a4a97acaee6
AngelCS94 <163451592+AngelCS94@users.noreply.github.com>
2024-06-20 12:32:06 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function fabricaexcusas() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car", "my head"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch"
+  ];
+  let myArray = [
+    who[getRandomInt()],
+    action[getRandomInt()],
+    what[getRandomInt()],
+    when[getRandomInt()]
+  ];
+  function getRandomInt() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    return randomNumber;
+  }
+
+  // return (
+  //   who[getRandomInt()] +
+  //   " " +
+  //   action[getRandomInt()] +
+  //   " " +
+  //   what[getRandomInt()] +
+  //   " " +
+  //   when[getRandomInt()]
+  // );
+  return myArray[0];
+}
+document.getElementById("excusa").innerHTML = fabricaexcusas();
